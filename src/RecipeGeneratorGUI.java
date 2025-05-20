e72ee15c43dbf2984b02e69ebc59a6f037f832d0
tnguye37 <tnguye37@ncsu.edu>
2025-05-20 15:44:56 -0400

    Initial commit

---
+++ src/RecipeGeneratorGUI.java
@@ -0,0 +1,16 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class RecipeGeneratorGUI extends JFrame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    // Main program to start the GUI
+    public static void main(String[] args) {
+        
+    }
+
+} 
